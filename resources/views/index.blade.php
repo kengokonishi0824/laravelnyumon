f7a7a3dda3ba7a49ed6b45c4235973df5d71914f
kengokonishi0824 <konishikengo0824@gamail.com>
2022-10-02 23:39:56 +0900

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="./../../public/css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Todo_list</title>
+</head>
+
+<body class=body>
 <form action="/add" method="post">
   <table>
     @csrf
@@ -22,25 +33,37 @@
 <table>
   <tr>
     <th>作成日</th>
-    <th>タスク名</th>
-    <th>更新</th>
+    <th>
+      <table>
+        <th>タスク名</th>
+        <th>更新</th>
+      </table>
+    </th>
     <th>削除</th>
   @foreach ($todos as $todo)
   <tr>
     <td>
       {{$todo->created_at}}
     </td>
-    <td>
-      <input type="text" name="content" value="{{$todo->content}}">
-    </td>
+      
     <td><form action="/edit" method="POST">
       @csrf
-      <button>更新</button>
+      <input type="text" name="content" value="{{$todo->content}}">
+      <input type ="hidden" name="id" value="{{$todo->id}}">
+      <input type="submit" value="更新">
+      </form>
     </td>
     <td><form action="/delete" method="POST">
       @csrf
-      <button>削除</button>
+      <input type ="hidden" name="id" value="{{$todo->id}}">
+      <input type="submit" value="削除">
+      </form>
     </td>
   </tr>
   @endforeach
 </table>
+
+
+
+</body>
+</html>
